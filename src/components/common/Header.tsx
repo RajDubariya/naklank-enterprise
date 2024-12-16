ab91f1a339428bfe9d834478995c60f0e0febba1
khushal <vorakhushal482@gmail.com>
2024-12-16 18:52:40 +0530

    heder done

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -12,16 +12,52 @@ const Header = (props: Props) => {
   const { header } = props;
 
   return (
-    <div className="bg-brand flex">
-      <Image
-        src={urlFor(header?.logo?.image)}
-        alt={header?.logo?.alt}
-        width={200}
-        height={200}
-      />
-      {header?.headerLinks?.map((item) => (
-        <Link href={item?.link}>{item?.label}</Link>
-      ))}
+    <div className="bg-brand flex items-center justify-between p-2 pr-6 pl-5">
+
+      <div className="flex items-center space-x-4">
+        <Image
+          src={urlFor(header?.logo?.image)}
+          alt={header?.logo?.alt}
+          width={90}
+          height={90}
+        />
+      </div>
+
+      <div className="flex space-x-6 relative">
+        {header?.headerLinks?.map((item, index) => (
+          <div key={index} className="relative group">
+            <Link
+              href={item?.link || "#"}
+              className="text-white text-lg font-medium relative group-hover:text-brand-light"
+            >
+              <span className="transition-transform duration-300 group-hover:scale-110">
+                {item?.label}
+              </span>
+              <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-brand-light group-hover:w-full transition-all duration-300"></span>
+            </Link>
+
+            {item?.dropdownLinks && item?.dropdownLinks.length > 0 && (
+              <div className="absolute left-0 mt-2 w-48 bg-white shadow-lg rounded-md opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition-opacity duration-500 z-50">
+                <ul className="py-2">
+                  {item?.dropdownLinks.map((dropdownItem, dropdownIndex) => (
+                    <li
+                      key={dropdownIndex}
+                      className="hover:bg-[#3e4095] transition-colors duration-300"
+                    >
+                      <Link
+                        href={dropdownItem.link}
+                        className="block px-4 py-2 text-sm text-gray-800 hover:text-white rounded-s-3xl"
+                      >
+                        {dropdownItem.label}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
