5de3e07f7c174e3705a8e26afb527c36ffd106d2
khushal <vorakhushal482@gmail.com>
2024-12-16 19:47:18 +0530

    header issue fix

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -12,7 +12,7 @@ const Header = (props: Props) => {
   const { header } = props;
 
   return (
-    <header className="bg-brand flex items-center justify-between p-4 px-5">
+    <div className="bg-brand flex items-center justify-between p-2 pr-6 pl-5">
       <div className="flex items-center space-x-4">
         <Image
           src={urlFor(header?.logo?.image)}
@@ -21,12 +21,13 @@ const Header = (props: Props) => {
           height={90}
         />
       </div>
+
       <div className="flex space-x-6 relative">
         {header?.headerLinks?.map((item, index) => (
           <div key={index} className="relative group">
             <Link
               href={item?.link || "#"}
-              className="text-white text-lg relative group-hover:text-brand-light"
+              className="text-white text-lg font-medium relative group-hover:text-brand-light"
             >
               <span className="transition-transform duration-300 group-hover:scale-110">
                 {item?.label}
@@ -34,19 +35,20 @@ const Header = (props: Props) => {
               <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-brand-light group-hover:w-full transition-all duration-300"></span>
             </Link>
 
+            {/* Dropdown Section */}
             {item?.dropdownLinks && item?.dropdownLinks.length > 0 && (
-              <div className="absolute left-0 mt-2 w-48 bg-white shadow-lg rounded-md opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition-opacity duration-500 z-50">
-                <ul className="py-2">
-                  {item?.dropdownLinks?.map((dropdownItem) => (
+              <div className="absolute left-0 mt-2 w-48 bg-white shadow-lg rounded-md opacity-0 group-hover:opacity-100 pointer-events-auto transition-opacity duration-300 z-50">
+                <ul className="py-2 group-hover:block">
+                  {item?.dropdownLinks.map((dropdownItem, dropdownIndex) => (
                     <li
-                      key={item?._key}
-                      className="hover:bg-brand transition-colors duration-300"
+                      key={dropdownIndex}
+                      className="hover:bg-[#3e4095] transition-colors duration-300 rounded-lg"
                     >
                       <Link
-                        href={dropdownItem?.link}
-                        className="block px-4 py-2 text-sm text-gray-800 hover:text-white rounded-s-3xl"
+                        href={dropdownItem.link}
+                        className="block px-4 py-2 text-sm text-gray-800 hover:text-white"
                       >
-                        {dropdownItem?.label}
+                        {dropdownItem.label}
                       </Link>
                     </li>
                   ))}
@@ -56,7 +58,7 @@ const Header = (props: Props) => {
           </div>
         ))}
       </div>
-    </header>
+    </div>
   );
 };
 
