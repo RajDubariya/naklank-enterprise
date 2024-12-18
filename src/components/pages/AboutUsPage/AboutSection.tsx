c5b1925979e6af151735f700152f348f610a7e26
raj <raj3043patel@gmail.com>
2024-12-18 23:16:22 +0530

    updated code

--- src/components/pages/AboutUsPage/AboutSection.tsx
+++ src/components/pages/AboutUsPage/AboutSection.tsx
@@ -1,32 +1,52 @@
-import { AboutSectionType } from "@/types/pages/AboutUsPage";
-import React from "react";
-import Image from "next/image";
+"use client";
 import { urlFor } from "@/sanity/lib/image";
+import { AboutSectionType, Item } from "@/types/pages/AboutUsPage";
+import { motion } from "framer-motion";
 import { PortableText } from "next-sanity";
+import Image from "next/image";
 
 const AboutSection = ({ data }: { data: AboutSectionType }) => {
-  console.log(data);
-
   return (
     <div className="py-12">
-   <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold text-center mb-12 relative">
-  <span className="inline-block pb-2 border-b-4 border-brand">
-    {data?.title}
-  </span>
-</h1>
+      <motion.h1
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+        viewport={{ once: true }}
+        className="text-3xl md:text-5xl lg:text-6xl font-bold text-center mb-12 relative"
+      >
+        <span className="inline-block pb-2 border-b-4 border-brand">
+          {data?.title}
+        </span>
+      </motion.h1>
 
       <div className="space-y-20">
-        {data?.items?.map((item: any, index: number) => (
-          <div
+        {data?.items?.map((item: Item, index: number) => (
+          <motion.div
             key={item?._key || index}
+            initial={{ opacity: 0, y: 10 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              duration: 0.4,
+              ease: "easeInOut",
+            }}
+            viewport={{ once: true }}
             className={`flex flex-col md:flex-row ${
               index % 2 === 0
                 ? "md:flex-row-reverse"
                 : "md:flex-row bg-gray-100 py-20"
-            } items-center p-6 px-12 gap-8`}
+            } items-center p-4 md:px-12 gap-8`}
           >
-            {/* Image Section */}
-            <div className="relative w-full md:w-1/2 h-64 md:h-96 overflow-hidden rounded-lg">
+            <motion.div
+              initial={{ opacity: 0, scale: 0.9 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.45,
+                ease: "easeInOut",
+              }}
+              viewport={{ once: true }}
+              className="relative w-full md:w-1/2 h-64 md:h-96 overflow-hidden rounded-lg"
+            >
               <Image
                 src={
                   item?.image?.image
@@ -37,21 +57,49 @@ const AboutSection = ({ data }: { data: AboutSectionType }) => {
                 layout="fill"
                 objectFit="contain"
               />
-            </div>
+            </motion.div>
 
-            {/* Text Section */}
             <div className="w-full md:w-1/2 space-y-4">
-              <h2 className="text-2xl md:text-3xl mb-3 lg:text-4xl md:mb-8 font-bold text-black uppercase">
+              <motion.h2
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                  duration: 0.45,
+                  ease: "easeInOut",
+                }}
+                viewport={{ once: true }}
+                className="text-2xl md:text-3xl mb-3 lg:text-4xl md:mb-8 font-bold text-black uppercase"
+              >
                 {item?.maintitle || "Main Title"}
-              </h2>
-              <h3 className="text-xl md:text-2xl lg:text-3xl font-semibold text-gray-600">
+              </motion.h2>
+
+              <motion.h3
+                initial={{ opacity: 0, y: 25 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                  duration: 0.45,
+                  ease: "easeInOut",
+                }}
+                viewport={{ once: true }}
+                className="text-xl md:text-2xl lg:text-3xl font-semibold text-gray-600"
+              >
                 {item?.title || "Section Title"}
-              </h3>
-              <p className="text-base md:text-base lg:text-lg text-gray-500">
+              </motion.h3>
+
+              <motion.p
+                initial={{ opacity: 0, y: 30 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                  duration: 0.45,
+                  ease: "easeInOut",
+                }}
+                viewport={{ once: true }}
+                className="text-base md:text-base lg:text-lg text-gray-500"
+              >
                 <PortableText value={item?.description} />
-              </p>
+              </motion.p>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
